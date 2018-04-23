0f76c182eb844275fc4204fe1f732f7f3d4dfd3b
ANUWAS <info.anuwas@gmail.com>
2018-04-23 21:57:46 +0530

    Development: Update Collection Spring Js 23/04/2018

--- resources/js/anuwastqa.js
+++ resources/js/anuwastqa.js
@@ -1,20 +1,15 @@
-/*! AdminLTE app.js
+/*! ANUWAS app.js
 * ================
-* Main JS application file for AdminLTE v2. This file
-* should be included in all pages. It controls some layout
-* options and implements exclusive AdminLTE plugins.
+* 
 *
-* @Author  Almsaeed Studio
-* @Support <https://www.almsaeedstudio.com>
-* @Email   <abdullah@almsaeedstudio.com>
-* @version 2.4.2
-* @repository git://github.com/almasaeed2010/AdminLTE.git
-* @license MIT <http://opensource.org/licenses/MIT>
+* @Author  ANUWAS
+* @Support <http://www.anuwas.com>
+* @Email   <info@anuwas.com>
 */
 
 // Make sure jQuery has been loaded
 if (typeof jQuery === 'undefined') {
-throw new Error('AdminLTE requires jQuery')
+throw new Error('ANUWAS requires jQuery')
 }
 
 /* BoxRefresh()
@@ -517,7 +512,7 @@ throw new Error('AdminLTE requires jQuery')
 
 /* Layout()
  * ========
- * Implements AdminLTE layout.
+ * Implements ANUWAS layout.
  * Fixes the layout height in case min-height fails.
  *
  * @usage activated automatically upon window load.
