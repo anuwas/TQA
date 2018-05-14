9cdbe2942e6c06628eeabb50b91e3a8337405b27
ANUWAS <info.anuwas@gmail.com>
2018-05-15 01:17:14 +0530

    commit 14/05/2018

---
+++ src/com/anu/tqa/proxyobjectlaxyegarfetch/MainClass.java
@@ -0,0 +1,25 @@
+package com.anu.tqa.proxyobjectlaxyegarfetch;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.anu.tqa.proxyobjectlaxyegarfetch.dto.Address;
+import com.anu.tqa.proxyobjectlaxyegarfetch.dto.UserDetail;
+
+
+
+public class MainClass {
+	public static void main(String[] args) {
+		UserDetail userDetail = new UserDetail();
+		
+		
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		
+		userDetail = (UserDetail)session.get(UserDetail.class, 1);
+		session.close();
+		System.out.println(userDetail.getListOfAddress().size());
+		
+	}
+}
---
+++ src/com/anu/tqa/proxyobjectlaxyegarfetch/dto/UserDetail.java
@@ -0,0 +1,49 @@
+package com.anu.tqa.proxyobjectlaxyegarfetch.dto;
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+
+@Entity
+public class UserDetail {
+
+	@Id
+	private int userId;
+	private String userName;
+	@ElementCollection(fetch=FetchType.EAGER)
+	@JoinTable(name="user_address",
+	joinColumns=@JoinColumn(name = "user_id")
+	)
+	private Set<Address> listOfAddress = new HashSet();
+	
+
+	public Set<Address> getListOfAddress() {
+		return listOfAddress;
+	}
+	public void setListOfAddress(Set<Address> listOfAddress) {
+		this.listOfAddress = listOfAddress;
+	}
+
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	
+}
